8b1cd6b6d595da795069c4e60bfa82a65160ced7
Tello <pateltanaka22@live.com>
2020-05-16 11:32:15 +0200

    updated the results

--- src/app/components/Patient/PatientDashboard.js
+++ src/app/components/Patient/PatientDashboard.js
@@ -60,7 +60,7 @@ const PatientDashboard = (props) => {
         
       otpExpired === true ? loc.push('/otp') : console.log('its now true')
         function generateQR() {
-            let str = "Maisha covid-19 test result :" + testingDetails.result
+            let str =  "Maisha covid-19 test result for  " +repo.firstName + ' with and ID of ' + repo.nationalIDNumber + ' result:' + testingDetails.result
             QRCode.toCanvas(document.getElementById('canvas'), str)
         }
         generateQR()
