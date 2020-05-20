16e20f83d89adffcf5cf68ffe955089bf05d297b
Tello <pateltanaka22@live.com>
2020-05-20 17:27:42 +0200

    error changes

--- src/app/components/Auth/Login.js
+++ src/app/components/Auth/Login.js
@@ -10,47 +10,49 @@ import Grid from '@material-ui/core/Grid';
 import Alert from '@material-ui/lab/Alert';
 import Snackbar from '@material-ui/core/Snackbar';
 
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import axios from 'axios'
 import api from '../../../utils/helpers/api';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
+    root: {
+        width: '100%',
+        '& > * + *': {
+            marginTop: theme.spacing(2)
+        }
+    }
 }));
-
-
-
 
 const Login = ({next}) => {
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [open, setOpen] = React.useState(false);
-  const [openError, setOpenError] = React.useState(false);
+    const [open,
+        setOpen] = React.useState(false);
+    const [openError,
+        setOpenError] = React.useState(false);
+    const [errorMessage,
+        setErrorMsage] = useState('')
 
     const [userData,
         setData] = useState({username: '', password: ''})
-    
 
-        const handleClick = () => {
-          setOpen(true);
-          
-        };
-        const handleClickError = () => {
-            setOpenError(true);
-          };
-      
-        const handleClose = (event, reason) => {
-          if (reason === 'clickaway') {
+    const handleClick = () => {
+        setOpen(true);
+
+    };
+    const handleClickError = (x) => {
+
+        setErrorMsage(x)
+        setOpenError(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
             return;
-          }
+        }
 
-          switch (Role) {
+        switch (Role) {
             case 'ADMIN':
                 return window.location.href = '/dashboard'
             case 'AGENT':
@@ -60,20 +62,20 @@ const Login = ({next}) => {
             default:
                 break;
         }
-      
-          setOpen(false);
-        
-        };
-      
-       const handleCloseError = (event, reason) => {
-            if (reason === 'clickaway') {
-              return;
-            }
-        
-            setOpenError(false);
-            
-          };
-      
+
+        setOpen(false);
+
+    };
+
+    const handleCloseError = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpenError(false);
+
+    };
+
     //console.log(userData)
     function handleSubmit(e) {
         e.preventDefault();
@@ -81,23 +83,19 @@ const Login = ({next}) => {
 
             axios
                 .post(`${api.authUrl}`, userData)
-                .then( async res => {
+                .then(async res => {
                     localStorage.setItem('access_token', res.data.jwtToken)
                     const decoded = jwt_decode(res.data.jwtToken);
                     var {Role, username} = decoded
                     localStorage.setItem('username', username)
                     localStorage.setItem('Role', Role)
-                   await handleClick()  
-
-                   
+                    await handleClick()
 
                 })
                 .catch(err => {
 
-                  console.log(err)
+                    handleClickError(err.response.data.message)
 
-                  handleClickError()
-                  
                 })
 
         }
@@ -122,29 +120,35 @@ const Login = ({next}) => {
 
             <div style={styles.img}/>
 
-
             <div className={classes.root}>
-      
-            <Snackbar open={open} autoHideDuration={3000} anchorOrigin={{
-vertical: "top",
-horizontal: "center"
-}} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success">
-                Logged in successfully
-              </Alert>
-            </Snackbar>
-         
-         
-            <Snackbar open={openError} anchorOrigin={{
-vertical: "top",
-horizontal: "center"
-}} autoHideDuration={3000} onClose={handleCloseError}>
-            <Alert onClose={handleCloseError} severity="error">
-              Invalid credentials! 
-            </Alert>
-          </Snackbar>
-          
-          </div>
+
+                <Snackbar
+                    open={open}
+                    autoHideDuration={3000}
+                    anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "center"
+                }}
+                    onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="success">
+                        Logged in successfully
+                    </Alert>
+                </Snackbar>
+
+                <Snackbar
+                    open={openError}
+                    anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "center"
+                }}
+                    autoHideDuration={3000}
+                    onClose={handleCloseError}>
+                    <Alert onClose={handleCloseError} severity="error">
+                        {errorMessage}
+                    </Alert>
+                </Snackbar>
+
+            </div>
 
             <Col
                 style={styles.container}
@@ -170,64 +174,60 @@ horizontal: "center"
                     marginTop: -30
                 }}
                     onSubmit={handleSubmit}>
-                   <div align="center">
-<FormGroup>
+                    <div align="center">
+                        <FormGroup>
 
-<TextField
-    label="Username"
-    name="username"
-    value={userData.username}
-    id="username"
-    placeholder="Username"
-    onChange={e => setData({
-    ...userData,
-    username: e.target.value
-})}
-    style={{
-    width: "50%"
-}}
-    required/>
-</FormGroup>
-<FormGroup>
+                            <TextField
+                                label="Username"
+                                name="username"
+                                value={userData.username}
+                                id="username"
+                                placeholder="Username"
+                                onChange={e => setData({
+                                ...userData,
+                                username: e.target.value
+                            })}
+                                style={{
+                                width: "50%"
+                            }}
+                                required/>
+                        </FormGroup>
+                        <FormGroup>
 
-<TextField
-    type="password"
-    label="Password"
-    name="password"
-    id="password"
-    value={userData.password}
-    placeholder="password"
-    onChange={e => setData({
-    ...userData,
-    password: e.target.value
-})}
-    style={{
-    width: "50%"
-}}
-    required/>
-</FormGroup>
+                            <TextField
+                                type="password"
+                                label="Password"
+                                name="password"
+                                id="password"
+                                value={userData.password}
+                                placeholder="password"
+                                onChange={e => setData({
+                                ...userData,
+                                password: e.target.value
+                            })}
+                                style={{
+                                width: "50%"
+                            }}
+                                required/>
+                        </FormGroup>
 
-<Button
-color="success"
-style={{
-width: "50%"
-}}
-type="submit">Submit</Button>
-                   </div>
+                        <Button
+                            color="success"
+                            style={{
+                            width: "50%"
+                        }}
+                            type="submit">Submit</Button>
+                    </div>
 
                 </Form>
 
-
-
                 <Grid item>
-                <Link href="/otp" variant="body2">
-                  {"Login as Patient😊"}
-                </Link>
-              </Grid>
+                    <Link href="/otp" variant="body2">
+                        {"Login as Patient😊"}
+                    </Link>
+                </Grid>
 
             </Col>
-
-            
 
         </div>
     );
