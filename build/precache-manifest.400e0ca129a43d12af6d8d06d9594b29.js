2a9ff935914ea100cff32f1a5c9e3b33996e8bcd
tinlee <tinashediego@gmail.com>
2020-05-21 17:31:18 +0200

    build

---
+++ build/precache-manifest.400e0ca129a43d12af6d8d06d9594b29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0b7f3f29825bc62517a7ea8822f631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "574fc1d630333aebd56a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c360408576a2ed392fb3",
+    "url": "/static/css/main.7ac6bb5b.chunk.css"
+  },
+  {
+    "revision": "574fc1d630333aebd56a",
+    "url": "/static/js/2.87879ba3.chunk.js"
+  },
+  {
+    "revision": "35313e7121485386b4274887a268f9ec",
+    "url": "/static/js/2.87879ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c360408576a2ed392fb3",
+    "url": "/static/js/main.02276776.chunk.js"
+  },
+  {
+    "revision": "41fd2f6b0df6992a98c2",
+    "url": "/static/js/runtime-main.b5b3f2f0.js"
+  },
+  {
+    "revision": "978c0cfadea0998dbdd58148bdaea8d4",
+    "url": "/static/media/1.978c0cfa.jpg"
+  }
+]);
